4ee1a2dd437a295b817d542b7464156e87e2d835
jo3e22 <jo3e22@soton.ac.uk>
2024-11-01 13:54:39 +0000

    working!

--- instantaneous_simulation.py
+++ instantaneous_simulation.py
@@ -4,14 +4,22 @@ from matplotlib import pyplot as plt
 import simulated_data_v2 as sim
 
 # %%
-Env = sim.Environment(30, 30)
-Object = sim.Object(shape=1, x_start=1, y_start=4)
+Env = sim.Environment(500, 500)
+Object = sim.Object(shape=20, x_start=(100+Env.border_width), y_start=(400+Env.border_width))
 
-map = sim.add_small_mask_to_large_mask(Env.map, Object.mask, Object.y_start, Object.x_start)
+map = sim.add_small_mask_to_large_mask(Env.map, Object.mask, Object.x_start, Object.y_start)
 
-#leftLidar = sim.Sensor(0+Env.border_width, 0+Env.border_width, -45, map)
+leftLidar = sim.Sensor(0+Env.border_width, 0+Env.border_width, 48/2, map)
+rightLidar = sim.Sensor(500-Env.border_width, 0+Env.border_width, -48/2, map)
+left_data = leftLidar.scan(map)
+right_data = rightLidar.scan(map)
 
 # %%
-plt.imshow(map, cmap = 'gray')
-plt.show()
+fig, ax = plt.subplots()
+ax.clear()
+ax.imshow(map, cmap='gray', interpolation='none', origin='lower')
+leftLidar.plot_detector(ax, left_data)
+rightLidar.plot_detector(ax, right_data)
+ax.axis('off')
+ax.set_title('Environment Map')
 # %%
